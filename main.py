7169c0c949d6470d7715b84f192f158d0af0d3a4
daniil <daniilkuksa@gmail.com>
2022-12-13 16:33:53 +0300

    Upgrade program

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtGui import QPainter, QColor, QPolygon
 import sys
 import random
+from design import Ui_MainWindow
 
 SCREEN_SIZE = [680, 480]
 
 
-class Example(QMainWindow):
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
         self.flag_ = False
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.draw)
         self.coords = []
 
@@ -20,8 +21,8 @@ class Example(QMainWindow):
         if self.flag_:
             qp = QPainter()
             qp.begin(self)
-            qp.setPen(QColor(self.color))
-            qp.setBrush(QColor(self.color))
+            qp.setPen(QColor(*self.color))
+            qp.setBrush(QColor(*self.color))
             self.x, self.y = random.randint(100, SCREEN_SIZE[0] - 100), random.randint(100, SCREEN_SIZE[1] - 100)
             if self.figure == 'circle':
                 qp.drawEllipse(self.x, self.y, self.size, self.size)
@@ -30,8 +31,8 @@ class Example(QMainWindow):
     def draw(self):
         self.figure = 'circle'
         self.size = random.randint(10, 100)
-        self.color = 'yellow'
-        self.flag_ = True
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))  # 'yellow'
+        self.flag = True
         self.update()
 
 
